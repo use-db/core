f71af1b7749f76ae3823291b5f120faffdb3cabe
Ankur Kedia <theankurkedia@gmail.com>
2020-10-01 19:05:46 +0530

    Exported md5

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export { db, RootQueryBuilder, QueryBuilder, QueryData } from './query';
 export { Connection } from './connection';
 export { Binding, RuntimeBinding } from './binding';
 export { Cache } from './cache';
+export { md5 } from './utils/md5';
