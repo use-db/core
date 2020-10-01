f6fa762f7771bca78444334353d63d6548c4b936
Ankur Kedia <theankurkedia@gmail.com>
2020-10-01 13:34:48 +0530

    Exported types

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { db, RootQueryBuilder, QueryBuilder } from './query';
 export { Connection } from './connection';
 export { Binding, RuntimeBinding } from './binding';
+export { Cache } from './cache';
